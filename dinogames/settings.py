e88004df4adffed121fdb6c5a82a60ae55b0ee46
ccp84 <ccp84@icloud.com>
2023-05-16 18:40:43 +0000

    py>update cookies for heroku

--- dinogames/settings.py
+++ dinogames/settings.py
@@ -148,8 +148,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
-        'rest_framework.authentication.SessionAuthentication'
-        # 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        # 'rest_framework.authentication.SessionAuthentication'
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )]
 }
 
