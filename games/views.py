310a258be434dd36595c3b8991c1a6fc1899d112
ccp84 <ccp84@icloud.com>
2023-05-23 20:38:10 +0000

    py>add filters to game view

--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Game
 from .serializers import GameSerializer
 from rest_framework import permissions
@@ -8,7 +8,22 @@ from dinogames.permissions import IsOwnerOrReadOnly
 class GameList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = GameSerializer
-    queryset = Game.objects.all()
+    queryset = Game.objects.all().order_by('-id')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        'title',
+        'tags',
+    ]
+    ordering_fields = [
+        'id',
+        'title',
+        'maxplayers',
+        'minplayers',
+        'playtime',
+    ]
 
 
 class GameDetail(generics.RetrieveUpdateAPIView):
