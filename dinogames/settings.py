e19cf29f8bc894dc613f527a3ece8c28cc2b989d
ccp84 <ccp84@icloud.com>
2023-05-24 13:48:19 +0000

    py>update cors origins

--- dinogames/settings.py
+++ dinogames/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     os.environ.get('CLIENT_ORIGIN')
 ]
