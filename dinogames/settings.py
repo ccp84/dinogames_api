9bd978a4f9c39608276eeab9161a242e85c393fc
ccp84 <ccp84@icloud.com>
2023-06-23 17:24:56 +0000

    py>remove allow all from cors settings

--- dinogames/settings.py
+++ dinogames/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     os.environ.get('CLIENT_ORIGIN')
 ]
